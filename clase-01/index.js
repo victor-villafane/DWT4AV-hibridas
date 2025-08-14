00c3963e49ea94307c722137adb882d9ba16b23e
victor-villafane <victor.villafane@davinci.edu.ar>
2025-08-14 17:56:32 -0300

    servidor muy basico

--- clase-01/index.js
+++ clase-01/index.js
@@ -1,1 +1,7 @@
-console.log("Hola desde node!!")
+const http = require("http")    //commonjs
+//import http from "http"    
+
+http.createServer( function(request, response){
+    console.log("Hola desde el servidor!", request.url)
+    response.end("llego la solicitud")
+} ).listen(2025)
